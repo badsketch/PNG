398a4d1f22ad5964e21efe7311105f066f18ee64
Nick Ma <nicholashma@gmail.com>
2018-10-09 17:02:37 -0700

    Mock out getWord() and change 'class' to 'className'

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,14 @@ import React, { Component } from "react";
 import './App.scss';
 
 class App extends Component {
+    getWord() {
+            console.log("yay");
+    }
     render() {
         return (
-            <div class="box has-text-centered">
-                <h1 class="title">Picture Naming Game</h1>
-                <button class="button is-danger">Click Me</button>
+            <div className="box has-text-centered">
+                <h1 className="title">Picture Naming Game</h1>
+                <button className="button is-danger" onClick={this.getWord}>Click Me</button>
             </div>
         )
     }
