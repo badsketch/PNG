7f119ab4761e84fd093cc79b9efd7cc6a243d150
danielwang405 <danielwang405@gmail.com>
2019-01-17 14:38:04 -0800

    #2 created reference to config file for environment variables like flickr api and set up initial fetch to a photo search query

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,50 @@
 import React, { Component } from "react";
-import './App.scss';
+import "./App.scss";
+import config from "../../config";
 
-import { ImageCanvas } from './image-canvas/ImageCanvas';
+import { ImageCanvas } from "./image-canvas/ImageCanvas";
 
 class App extends Component {
-    getWord() {
-            console.log("yay");
-    }
-    render() {
-        return (
-            <div>
-                <div className="box has-text-centered">
-                    <h1 className="title">Picture Naming Game</h1>
-                    <button className="button is-danger" onClick={this.getWord}>Click Me</button>
-                </div>
-                <div className="pictureArea">
-                    <div className="canvas">
-                        <ImageCanvas></ImageCanvas> 
-                    </div>
-                </div>
-            </div>
-            )
-    }
+  constructor(props) {
+    super(props);
+    this.getImage = this.getImage.bind(this);
+  }
+  componentDidMount() {
+    // integrate with getWord
+    this.getImage();
+  }
+  getWord() {
+    console.log("yay");
+  }
+
+  getImage() {
+    let API_KEY = config.FLICKR_API_KEY;
+
+    fetch(
+      `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=duck&format=json&nojsoncallback=true`
+    )
+      .then(res => res.json())
+      .then(res => {
+        console.log(res.photos);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <div className="box has-text-centered">
+          <h1 className="title">Picture Naming Game</h1>
+          <button className="button is-danger" onClick={this.getWord}>
+            Click Me
+          </button>
+        </div>
+        <div className="pictureArea">
+          <div className="canvas">
+            <ImageCanvas />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
