08c7602c3b980e217879967f2019f806dcd41f88
DanielWang405 <Danielwang405@gmail.com>
2019-01-26 15:00:08 -0800

    updated tests to account for game arena status

--- src/components/game-arena/GameArena.test.js
+++ src/components/game-arena/GameArena.test.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { GameArena } from "./GameArena";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 import "isomorphic-fetch";
+
+let statusENUM = {
+  READY: 1,
+  IN_PROGRESS: 2,
+  DONE: 3
+};
 
 describe("GameArena Component", () => {
   it("does not display an imageCanvas unless there is an image fetched", () => {
@@ -9,9 +15,25 @@ describe("GameArena Component", () => {
     let imageCanvas = wrapper.find("ImageCanvas");
     expect(imageCanvas.length).toBe(0);
     wrapper.setState({
+      status: statusENUM.IN_PROGRESS,
       imageUrl: "testUrl.jpg"
     });
     imageCanvas = wrapper.find("ImageCanvas");
     expect(imageCanvas.length).toBe(1);
+  });
+
+  it("displays 'Start!' button when status is on ready", () => {
+    const wrapper = shallow(<GameArena />);
+    const text = wrapper.find("button").text();
+    expect(text).toEqual("Start!");
+  });
+
+  it("displays 'Play Again' button when game is over", () => {
+    const wrapper = shallow(<GameArena />);
+    wrapper.setState({
+      status: statusENUM.DONE
+    });
+    const text = wrapper.find("button").text();
+    expect(text).toEqual("Play Again");
   });
 });
